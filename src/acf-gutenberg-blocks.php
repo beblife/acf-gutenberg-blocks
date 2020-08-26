8780c90084d262edf9756dd11fa430a5b07e819a
Laurens Bultynck <laurens@starringjane.com>
2020-08-26 17:07:27 +0200

    Add plugin skeleton

---
+++ src/acf-gutenberg-blocks.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: ACF Gutenberg Blocks
+ * Description: A plugin that provides an elegant way to register Gutenberg blocks using ACF PRO.
+ * Version: 1.0
+ * Author: Laurens Bultynck
+ */
