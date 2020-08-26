651d35ca700d9359d4ed4ce292b9d94a053892dc
Laurens Bultynck <laurens@starringjane.com>
2020-08-26 23:42:54 +0200

    Update plugin info

--- gutenberg-blocks.php
+++ gutenberg-blocks.php
@@ -1,9 +1,13 @@
 <?php
 /**
  * Plugin Name: ACF Gutenberg Blocks
- * Description: A plugin that provides an elegant way to register Gutenberg blocks using ACF PRO.
- * Version: 1.0
+ * Description: Provides an elegant way for developers to register Gutenberg blocks using the Advanced Custom Fields PRO blocks feature.
+ * Version: 1.0.0
  * Author: Laurens Bultynck
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require "vendor/autoload.php";
